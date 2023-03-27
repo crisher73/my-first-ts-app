dd881fa77e259f5c400f57b4b7ccd9c1e57ef3dd
Cristina Hernández <achh17@gmail.com>
2023-03-27 18:08:53 -0500

    I added a observer

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,42 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-type Props = {image:string};
+type Props = {
+  image: string
+};
 
-export const RandomFox = ({image}:Props): JSX.Element => {
-  const node = useRef<HTMLImageElement>(null)
-  
-  return ( <img width="320" height="auto" src={image} className="rounded"/>
+export const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+  useEffect(() => {
+    //nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting || !node.current) {
+          return;
+        }
+
+        setSrc(image);
+      });
+    });
+
+    //observe node 
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    //desconectar
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
+  return (
+    <img
+      ref={node}
+      width="320"
+      height="auto"
+      src={src}
+      className="mx-auto rounded-md bg-gray-300" />
   );
 }
