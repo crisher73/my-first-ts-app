4147a3bfe776aa99bf272cc3ede4d11df4970945
Cristina Hernández <achh17@gmail.com>
2023-03-27 17:04:40 -0500

    I added a hook, (useRef) to take the value of the element

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,6 +1,10 @@
+import { useRef } from "react";
+
 type Props = {image:string};
 
-export function RandomFox({image}:Props): JSX.Element {
+export const RandomFox = ({image}:Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null)
+  
   return ( <img width="320" height="auto" src={image} className="rounded"/>
   );
 }
