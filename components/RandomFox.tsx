5963a7634d5cb2ee3bad8137014fb7a96fb6412c
Cristina Hernández <achh17@gmail.com>
2023-03-27 11:27:42 -0500

    I added type Props for a random image.

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,15 +1,6 @@
-// Generates a random number between 1 and 122
-const randomNumber = () => Math.floor(Math.random() * 122) + 1;
+type Props = {image:string};
 
-export function RandomFox(): JSX.Element {
-  const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
-
-  return (
-    <img
-      width="320"
-      height="auto"
-      src={image}
-      className="mx-auto rounded-md bg-gray-300"
-    />
+export function RandomFox({image}:Props): JSX.Element {
+  return ( <img width="320" height="auto" src={image} className="rounded"/>
   );
 }
